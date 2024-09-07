6a71fa140b3f4801338dc03054f1225fcdb673e9
Aswin Reji <aswinreji.004@gmail.com>
2024-09-07 14:16:20 +0000

    feat: learning the concept behind redux

--- src/home.tsx
+++ src/home.tsx
@@ -1,12 +1,26 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import slice, { addItem, clearItem } from "./utils/slice"
 
 export default function Home (){
 
   // We are subscribing to store using selector
   const items = useSelector((store)=>store.slice.items)
+  const dispatch = useDispatch()
+  function handleClearCart(){
+    dispatch(clearItem()) 
+  }
+  function handleAddItem(){
+    dispatch(addItem("pizza"))
+  }
   return (
     <>
-    <h1>Items: {items.length} </h1>
+    <button onClick={handleAddItem}>Add Item</button>
+    <button onClick={handleClearCart}>Clear Cart</button>
+    <h1>Items: {items.length === 0 ? "Cart is empty":items.length } </h1>
+    <ul>
+
+      {items.map((item:string)=><li>{item}</li>)}
+      </ul>
     <h1>Hello This is home page</h1>
     </>
   )
